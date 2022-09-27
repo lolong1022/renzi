bd95da2402cde3c23e01ce71d6f22b026be9bb54
jsli <2636530377@qq.com>
2022-09-27 12:07:22 +0800

    feat:路由配置完成

---
+++ src/views/yemian/attendances/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    departments
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Departments',
+
+  data() {
+    return {
+
+    }
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/yemian/salarys/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    salarys
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Salarys',
+
+  data() {
+    return {
+
+    }
+  },
+
+  mounted() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
